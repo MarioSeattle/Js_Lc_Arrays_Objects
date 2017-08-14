cb4965f3b596c2c32f8af52ffdc90bc552999f00
MarioSeattle <marioseattle@live.com>
2017-08-14 12:11:14 -0700

    Testing all diferent kindas of functions

--- function_Lynda.js
+++ function_Lynda.js
@@ -1,3 +1,4 @@
+//Named Funcion
 function findBiggestFraction(a,b) {
     var result;
     a>b ? result = ["firstFraction", a] : result = ["secondFraction", b];
@@ -12,3 +13,41 @@ var fractionResult = findBiggestFraction(firstFraction,secondFraction);
 console.log("First fraction result: ", firstFraction);
 console.log("Second fraction result: ", secondFraction);
 console.log("Fraction " + fractionResult[0] + " with a value of " + fractionResult[1] + " is the biggest!");
+
+//Anonimous Function
+
+var a = 3/4;
+var b = 5/7;
+
+var theBiggestFunction = function(){
+    var result;
+    a>b ? result = ["a", a] : result = ["b", b];
+    console.log(result);
+
+}
+theBiggestFunction();
+
+//Using return
+
+var a = 3/4;
+var b = 5/7;
+
+var theBiggestFunction = function(){
+    var result;
+    a>b ? result = ["a", a] : result = ["b", b];
+    return result;
+
+}
+console.log(theBiggestFunction());
+
+//using parameters
+
+var theBiggestFunction = function(a,b){
+    var result;
+    a>b ? result = ["a", a] : result = ["b", b];
+    return result;
+
+}
+console.log(theBiggestFunction(3/4, 5/7));
+
+
