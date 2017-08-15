f3cff49470c59eac1ec98e8ff938457558738125
MarioSeattle <marioseattle@live.com>
2017-08-15 11:09:28 -0700

    Add multiply function and call variable theResult as a function

--- Closures_Lynda.js
+++ Closures_Lynda.js
@@ -1,3 +1,15 @@
-/**
- * Created by Mgomez on 8/15/17.
- */
+function doSomeMath() {
+    var a = 5;
+    var b = 4;
+
+    function multiple(){
+        var result = a*b;
+        return result;
+
+    }
+    return multiple;
+}
+
+var theResult = doSomeMath();
+
+console.log("The result: ", theResult());
